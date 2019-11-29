a11567fde996a313654fc027616aa2c115714585
Sébastien Tachier <sebastien.tachier@inforsud.com>
2019-11-29 08:27:47 +0100

    Exercice and comm

---
+++ Section7_Exercices/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <list-server></list-server>
+            <server-detail></server-detail>
+        </div>
+        <hr>
+        <div class="row">
+            <app-footer></app-footer>
+        </div>
+    </div>
+</template>
+
+<script>
+import AppHeader from './components/shared/Header.vue'
+import ListServer from './components/server/ListServer.vue'
+import ServerDetail from './components/server/ServerDetail.vue'
+import AppFooter from './components/shared/Footer.vue'
+
+
+export default {
+    components: {
+        'app-header': AppHeader,
+        'list-server': ListServer,
+        'server-detail': ServerDetail,
+        'app-footer': AppFooter
+    }
+}
+</script>
+
+<style>
+
+</style>
